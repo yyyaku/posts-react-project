65f01299e82e5fbd046dfa8939896b2900a9c56c
Артемий <artema121rus@mail.ru>
2023-10-02 16:03:37 +0300

    Добавляет модальное окно и кастомные хуки

--- src/App.js
+++ src/App.js
@@ -2,53 +2,45 @@ import React, { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/Myselect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./components/hooks/usePost";
+import mocks from "./components/mocks";
 
 function App() {
-  const [posts, setPosts] = useState ([
-    {id: 1, title: 'ааа', body: 'ддд'},
-    {id: 2, title: 'ллл', body: 'ггг'},
-    {id: 3, title: 'ввв', body: 'ввв'}
-  ])
+  const [posts, setPosts] = useState (mocks)
 
-  const [selectedSort, setSelectedSort] = useState('')
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
+  const sortedSearchAndPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p=>p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-    setPosts([...posts].sort((a,b)=>a[sort].localeCompare(b[sort])))
-  }
-
   return (
     <div className="App">
+      <MyButton onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal
+      visible={modal}
+      setVisible={setModal}
+      >
       <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}} />
-      <div>
-        <MySelect 
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultName="Сортировка"
-        options={[
-          {value: 'title', name: 'по названию'},
-          {value: 'body', name: 'по описанию'},          
-        ]}
-        />
-      </div>
-      {posts.length !== 0
-      ? 
-      <PostList remove={removePost} posts={posts} title="Посты про JS"/>
-      : 
-      <h1 style={{textAlign:"center"}}>
-        Посты не найдены!
-      </h1>
-      }
+      <PostFilter 
+      filter={filter}
+      setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedSearchAndPosts} title="Посты про JS"/>
     </div>
   );
 }
---
+++ src/components/UI/modal/MyModal.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./MyModal.module.css";
+
+function MyModal({ children, visible, setVisible }) {
+  const rootClasses = [classes.myModal];
+
+  if (visible) {
+    rootClasses.push(classes.active);
+  }
+
+  return (
+    <div className={rootClasses.join(" ")} onClick={() => setVisible(false)}>
+      <div
+        className={classes.myModalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default MyModal;
