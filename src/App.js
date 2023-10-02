06d8f010270b73fd433ce7073939a37dc54a6339
Артемий <artema121rus@mail.ru>
2023-10-02 16:56:55 +0300

    Добавляет загрузку данных из сервера

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,14 +6,17 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/usePost";
-import mocks from "./components/mocks";
+import PostService from "./components/API/PostService";
 
 function App() {
-  const [posts, setPosts] = useState (mocks)
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
   const sortedSearchAndPosts = usePosts(posts, filter.sort, filter.query)
+
+  useEffect(()=>{
+    fetchPosts();
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -24,8 +27,14 @@ function App() {
     setPosts(posts.filter(p=>p.id !== post.id))
   }
 
+  async function fetchPosts() {
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  }
+
   return (
     <div className="App">
+      <button onClick={fetchPosts}>get post</button>
       <MyButton onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
--- src/components/mocks.js
+++
@@ -1,7 +0,0 @@
-const mocks = [
-    {id: 1, title: 'ааа', body: 'ддд'},
-    {id: 2, title: 'ммм', body: 'ффф'},
-    {id: 5, title: 'ддд', body: 'ууу'}
-]
-
-export default mocks
