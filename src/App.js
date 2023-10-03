1534e06bf93967484798338f6514ef2264c94ff9
Артемий <artema121rus@mail.ru>
2023-10-03 22:22:05 +0300

    Добавляет fix в App.js с Link to

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./styles/App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import About from "./components/pages/About";
 import Posts from "./components/pages/Posts";
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/posts",
-      element: <Posts />,
-    },
-  ]);
  
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <BrowserRouter>
+      <div>
+        <div>
+          <Link to="/about">О сайте</Link>
+          <Link to="/posts">Посты</Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
