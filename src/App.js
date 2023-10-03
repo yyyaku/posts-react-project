c773bf5e587fc1073959792934ea5b150e776c1e
Артемий <artema121rus@mail.ru>
2023-10-03 21:14:41 +0300

    hzzz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from "react";
 import "./styles/App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Posts from "./components/pages/Posts";
 import About from "./components/pages/About";
+import Nav from "./components/UI/nav/Nav";
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/posts",
-      element: <Posts />,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/about",
+  //     element: <About />,
+  //   },
+  //   {
+  //     path: "/posts",
+  //     element: <Posts />,
+  //   },
+  // ]);
 
   return (
-      <div>
-        <RouterProvider router={router} />
-      </div>
+      // <div>
+      //   <Nav />
+      //   <RouterProvider router={router} />
+      // </div>
+     <div>
+      <Nav />
+      <Router>
+          <Route path="/about" component={About} />
+          <Route path="/posts" component={Posts} />
+      </Router>
+    </div>
   );
 }
 
---
+++ src/components/UI/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div className="nav">
+            <div className="nav__links">
+                <a href="/about">О сайте</a>
+                <a href="/posts">Посты</a>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
